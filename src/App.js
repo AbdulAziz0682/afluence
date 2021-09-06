6ce760a73978a892f5e6324186568c6fb21309ef
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-09-06 14:57:30 +0500

    Basic project setup with custom theme and material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { createGenerateClassName, jssPreset, StylesProvider } from '@material-ui/core/styles';
+import {create} from 'jss';
+import jssExtend from 'jss-plugin-extend';
+import rtl from 'jss-rtl';
+
+const customTheme = createTheme({
+	palette: {
+		type: 'light',
+		primary: {
+			main: '#1F2B36',
+		},
+		secondary: {
+			main: '#ff004d',
+			light: '#f1f1f1',
+		},
+	},
+})
+
+const jss = create({
+	...jssPreset(),
+	plugins: [...jssPreset().plugins, jssExtend(), rtl()],
+	insertionPoint: document.getElementById('jss-insertion-point')
+});
+
+export default function App(){
+	return (
+		<StylesProvider jss={jss} generateClassName={createGenerateClassName()}>
+			<ThemeProvider theme={customTheme}>
+		  		<h1 className="text-green-200 bg-gray-600">Afluence Setup with material Ui<Button variant="contained" color="primary" >BTN</Button></h1>
+			</ThemeProvider>
+		</StylesProvider>
+	)
 }
-
-export default App;
