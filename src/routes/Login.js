6c350eb4c1870cd91fed6d71ecc9054292669163
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-09-12 14:53:15 +0500

    Updated Login.js Styles

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,9 +3,11 @@ import { Button, Grid, InputAdornment, Typography } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 import { Hidden } from '@material-ui/core';
 
+import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
+
 import logo from '../assets/afluence.png';
 import registerBackground from '../assets/registerBackground.png';
-import account from '../assets/account.svg';
+import account from '../assets/account.png';
 import key from '../assets/key.svg';
 
 import { useFormik } from 'formik';
@@ -156,35 +158,118 @@ export default function Login(props){
         return <Redirect push to="/projects" />
     }
     return (
-        <Grid item className="mt-3">
-            <form onSubmit={formik.handleSubmit} className="w-100">
+        <Grid container item style={{height: 'calc(100vh - 64px)'}} alignItems="center" justifyContent="center">
+            <form onSubmit={formik.handleSubmit} className="w-full flex items-center justify-center">
             <Hidden smDown>
-                <Grid container id="md" direction="row" className="border w-full self-center rounded-lg mt-3 p-3">
-                    <Grid item className="flex flex-col justify-center p-6">
-                        <div className="flex flex-row items-center">
-                            <img src={logo} alt="afluence logo" width="50px"/>
-                            <Typography variant="h3" color="primary" className="font-extrabold text-right">Amazethu</Typography>
+                <Grid container id="md" direction="row" className="border lg:w-7/12 xl:5/12 self-center rounded-3xl mt-3">
+                    <Grid item xs={6} className="flex flex-col justify-center items-center p-5 bg-gray-100 rounded-l-3xl">
+                        <div className="flex flex-row items-center mt-9">
+                            <img src={logo} alt="afluence logo" style={{width: 70}}/>
+                            <Typography color="primary" className="font-extrabold text-4xl ml-2" style={{fontFamily: "'Montserrat', sans-serif"}}>amazethu</Typography>
                         </div>
-                        <img src={registerBackground} alt="register background" style={{width: '300px'}} />
+                        <img src={registerBackground} alt="register background" style={{width: 250}} />
                     </Grid>
-                    <Grid item>
-                        <Grid container spacing={2} direction="column" alignItems="center" justifyContent="center">
-                            <Grid item className="mt-8 mb-4 flex flex-col items-center">
-                                <Typography variant="h4" color="primary" className="font-extrabold">Welcome Back</Typography>
-                                <hr className="border-2 w-full border-black" />
+                    <Grid item xs={6}>
+                        <Grid container direction="column" alignItems="center" justifyContent="center" className="p-6">
+                            <Grid item className="flex flex-col items-center w-full">
+                                <Typography color="primary" className="font-black uppercase text-2xl">Welcome Back !</Typography>
+                                <hr className="border-b border-black w-11/12" />
+                                <hr className="border-b border-black w-10/12 my-1" />
+                                <hr className="border-b border-black w-9/12" />
                             </Grid>
-                            <Grid item className="w-full">
+                            <Grid container alignItems="center" direction="column" className="my-24">
+                                <Grid item className="w-full my-2">
+                                    <TextField
+                                        fullWidth
+                                        id="email"
+                                        name="email"
+                                        variant="standard"
+                                        label="Email"
+                                        size="small"
+                                        InputProps={{
+                                            startAdornment:(
+                                                <InputAdornment postition="start">
+                                                    <img src={account} alt="account icon" />
+                                                </InputAdornment>
+                                            ),
+                                            endAdornment:(
+                                                (formik.touched.email && Boolean(formik.errors.email)) &&
+                                                <InputAdornment position="end">
+                                                    <ReportProblemOutlinedIcon color="secondary" />
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                        value={formik.values.email}
+                                        onChange={formik.handleChange}
+                                        error={formik.touched.email && Boolean(formik.errors.email)}
+                                        helperText={formik.touched.email && formik.errors.email}
+                                    />
+                                </Grid>
+                                <Grid item className="w-full my-2">
+                                    <TextField
+                                        fullWidth
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        variant="standard"
+                                        label="Password"
+                                        size="small"
+                                        InputProps={{
+                                            startAdornment:(
+                                                <InputAdornment postition="start">
+                                                    <img src={key} alt="password key" />
+                                                </InputAdornment>
+                                            ),
+                                            endAdornment:(
+                                                (formik.touched.password && Boolean(formik.errors.password)) &&
+                                                <InputAdornment position="end">
+                                                    <ReportProblemOutlinedIcon color="secondary" />
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                        value={formik.values.password}
+                                        onChange={formik.handleChange}
+                                        error={formik.touched.password && Boolean(formik.errors.password)}
+                                        helperText={formik.touched.password && formik.errors.password}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={8} className="flex flex-col items-center">
+                                <Button variant="contained" color="primary" type="submit" fullWidth>Login</Button>
+                                <div className="w-80 flex justify-center items-center text-xs mt-2">
+                                    <span>Don't have an account?</span>
+                                    <Link to="/register" className="font-bold uppercase mx-1">Sign up</Link>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                </Hidden>
+                <Hidden mdUp>
+                    <Grid container spacing={2} alignItems="center" direction="column" className="self-center">
+                        <Grid item className="w-80 flex items-center p-6">
+                            <img src={logo} alt="logo" style={{width: 100}}/>
+                            <Typography variant="h4" color="primary" style={{fontFamily: "'Montserrat', sans-serif"}}>amazethu</Typography>
+                        </Grid>
+                        <Grid container alignItems="center" direction="column" className="my-24" spacing={3}>
+                            <Grid item className="w-80 my-2">
                                 <TextField
                                     fullWidth
                                     id="email"
                                     name="email"
-                                    variant="outlined"
+                                    variant="standard"
                                     label="Email"
                                     size="small"
                                     InputProps={{
                                         startAdornment:(
                                             <InputAdornment postition="start">
                                                 <img src={account} alt="account icon" />
+                                            </InputAdornment>
+                                        ),
+                                        endAdornment:(
+                                            (formik.touched.email && Boolean(formik.errors.email)) &&
+                                            <InputAdornment position="end">
+                                                <ReportProblemOutlinedIcon color="secondary" />
                                             </InputAdornment>
                                         )
                                     }}
@@ -194,19 +279,25 @@ export default function Login(props){
                                     helperText={formik.touched.email && formik.errors.email}
                                 />
                             </Grid>
-                            <Grid item className="w-full">
+                            <Grid item className="w-80 my-2">
                                 <TextField
                                     fullWidth
                                     type="password"
                                     id="password"
                                     name="password"
-                                    variant="outlined"
+                                    variant="standard"
                                     label="Password"
                                     size="small"
                                     InputProps={{
                                         startAdornment:(
                                             <InputAdornment postition="start">
                                                 <img src={key} alt="password key" />
+                                            </InputAdornment>
+                                        ),
+                                        endAdornment:(
+                                            (formik.touched.password && Boolean(formik.errors.password)) &&
+                                            <InputAdornment position="end">
+                                                <ReportProblemOutlinedIcon color="secondary" />
                                             </InputAdornment>
                                         )
                                     }}
@@ -216,72 +307,13 @@ export default function Login(props){
                                     helperText={formik.touched.password && formik.errors.password}
                                 />
                             </Grid>
-                            <Grid item className="w-full flex justify-center">
-                                <Button variant="contained" color="primary" type="submit">Login</Button>
-                            </Grid>
-                            <Grid item className="w-full flex justify-center items-center">
-                                <Typography variant="subtitle1">Don't have an account!</Typography>
-                                <Link to="/register" className="font-bold">Register</Link>
-                            </Grid>
                         </Grid>
-                    </Grid>
-                </Grid>
-                </Hidden>
-                <Hidden mdUp>
-                    <Grid container spacing={2} alignItems="center" direction="column" className="border self-center rounded-lg">
-                        <Grid item className="w-80 md:w-full flex items-center p-6">
-                            <img src={logo} alt="afluence logo" width="30%"/>
-                            <Typography variant="h3" color="primary" className="font-extrabold text-right">Amazethu</Typography>
-                        </Grid>
-                        <Grid item className="w-80 md:w-full">
-                            <TextField
-                                fullWidth
-                                id="email"
-                                name="email"
-                                variant="outlined"
-                                label="Email"
-                                size="small"
-                                InputProps={{
-                                    startAdornment:(
-                                        <InputAdornment postition="start">
-                                            <img src={account} alt="account icon" />
-                                        </InputAdornment>
-                                    )
-                                }}
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                error={formik.touched.email && Boolean(formik.errors.email)}
-                                helperText={formik.touched.email && formik.errors.email}
-                            />
-                        </Grid>
-                        <Grid item className="w-80 md:w-full">
-                            <TextField
-                                fullWidth
-                                type="password"
-                                id="password"
-                                name="password"
-                                variant="outlined"
-                                label="Password"
-                                size="small"
-                                InputProps={{
-                                    startAdornment:(
-                                        <InputAdornment postition="start">
-                                            <img src={key} alt="password key" />
-                                        </InputAdornment>
-                                    )
-                                }}
-                                value={formik.values.password}
-                                onChange={formik.handleChange}
-                                error={formik.touched.password && Boolean(formik.errors.password)}
-                                helperText={formik.touched.password && formik.errors.password}
-                            />
-                        </Grid>
-                        <Grid item className="w-80 md:w-full flex justify-center">
-                            <Button variant="contained" color="primary" type="submit">Login</Button>
-                        </Grid>
-                        <Grid item className="w-80 md:w-full flex justify-center items-center">
-                            <Typography variant="subtitle1">Don't have an account!</Typography>
-                            <Link to="/register" className="font-bold">Register</Link>
+                        <Grid item xs={8} className="flex flex-col items-center">
+                            <Button variant="contained" color="primary" type="submit" fullWidth>Login</Button>
+                            <div className="w-80 flex justify-center items-center text-xs mt-2">
+                                <span>Don't have an account?</span>
+                                <Link to="/register" className="font-bold uppercase mx-1">Sign up</Link>
+                            </div>
                         </Grid>
                     </Grid>
                 </Hidden>
