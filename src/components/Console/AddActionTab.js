acd89d660092d8672bfca69ff36b4d3f17386853
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-09-16 00:10:44 +0500

    Updated styles of AddActionTab

--- src/components/Console/AddActionTab.js
+++ src/components/Console/AddActionTab.js
@@ -1,15 +1,22 @@
 
 import {Grid, TextField, Button, Typography, Table, TableHead, TableRow, TableCell, TableBody, IconButton, FormControl, Select, MenuItem} from '@material-ui/core';
 import AddCircle from '@material-ui/icons/AddCircle';
-import DeleteIcon from '@material-ui/icons/Delete'
+import DeleteIcon from '@material-ui/icons/Delete';
+import NegativeIcon from '../../assets/negative.png';
 
 export default function AddActionTab(props){
 
     return (
         <Grid container direction="column" className="border rounded-lg overflow-scroll sm:overflow-auto">
-            <Grid item className="flex gap-3 p-1 sm:p-3 border-b-2 items-center justify-between flex flex-col sm:flex-row">
-                <TextField variant="outlined" label="Enter Action Name" size="small" />
-                <div className="flex gap-1 sm:justify-end w-full items-center justify-around">
+            <Grid item className="flex gap-3 p-1 sm:p-3 border-b items-center justify-between flex flex-col sm:flex-row">
+                <TextField 
+                    label="Enter action Name"
+                    variant="filled" 
+                    size="small"  
+                    className="w-full sm:w-5/12"
+                    InputProps={{disableUnderline: true}}
+                />
+                <div className="flex gap-9 sm:justify-end w-full items-center justify-between">
                     <Button variant="contained" color="secondary">Cancel</Button>
                     <Button variant="contained" color="primary">Save</Button>
                 </div>
@@ -19,35 +26,28 @@ export default function AddActionTab(props){
                     <Grid item className="flex flex-col">
                         <Typography variant="h4">Parameters List</Typography>
                     </Grid>
-                    <Grid item>
-                        <Table size="small">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell><Typography variant="subtitle2">Name</Typography></TableCell>
-                                    <TableCell><Typography variant="subtitle2">Data type</Typography></TableCell>
-                                    <TableCell><IconButton><AddCircle /></IconButton></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell>
-                                        <TextField variant="outlined" size="small" className="md:min-w-full" style={{minWidth: '100px'}} />
-                                    </TableCell>
-                                    <TableCell>
-                                        <FormControl variant="outlined" size="small" className="md:min-w-full" style={{minWidth: '100px'}}>
-                                            <Select value="string">
-                                                <MenuItem value="string">String</MenuItem>
-                                                <MenuItem value="number">Number</MenuItem>
-                                                <MenuItem value="bool">Boolean</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                    </TableCell>
-                                    <TableCell>
-                                        <IconButton><DeleteIcon color="error" /></IconButton>
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
+                    <Grid item container direction="column">
+                        <Grid item className="flex w-full gap-1 sm:gap-6">
+                            <Typography variant="h6" className="w-5/12 py-1">Name</Typography>
+                            <Typography variant="h6" className="w-5/12 py-1">Data type</Typography>
+                            <IconButton className="w-2/12"><AddCircle /></IconButton>
+                        </Grid>
+                        <Grid item className="flex w-full gap-1 sm:gap-6">
+                            <TextField 
+                                variant="filled" 
+                                size="small"  
+                                className="w-5/12"
+                                InputProps={{disableUnderline: true}}
+                            />
+                            <FormControl variant="filled" size="small" className="w-5/12">
+                                <Select value="string" disableUnderline>
+                                    <MenuItem value="string">String</MenuItem>
+                                    <MenuItem value="number">Number</MenuItem>
+                                    <MenuItem value="bool">Boolean</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <IconButton className="w-2/12"><img src={NegativeIcon} style={{width: 22}} alt="delete" /></IconButton>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
