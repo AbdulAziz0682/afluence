578326cb95852a9f36128135651e4710ab563e0e
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-09-17 23:11:30 +0500

    Updated EditSateTab and addtoactionlist in DrawerData

--- src/components/Console/DrawerData.js
+++ src/components/Console/DrawerData.js
@@ -1,6 +1,6 @@
-import { FormControl, IconButton, MenuItem, Select, Typography } from "@material-ui/core";
+import { FormControl, IconButton, MenuItem, Select, Typography, InputBase } from "@material-ui/core";
 import AddCircle from "@material-ui/icons/AddCircle";
-import Delete from '@material-ui/icons/Delete';
+import NegativeIcon from '../../assets/negative.png';
 
 export default function DrawerData({data}){
     if(!data) return 'No data';
@@ -15,31 +15,29 @@ export default function DrawerData({data}){
                                     <MenuItem value="add_action_to_list">add_action_to_list</MenuItem>
                                 </Select>
                             </FormControl>
-                            <div className="w-full flex flex-col">
-                                <div className="w-full flex gap-1 justify-between items-center">
-                                    <Typography variant="subtitle1">Action</Typography>
-                                    <Typography varint="subtitle1">Parameters</Typography>
-                                    <IconButton><AddCircle color="primary" /></IconButton>
+                            <div className="flex flex-col w-full">
+                                <div className="flex items-center w-full gap-1 sm:gap-10 mt-5">
+                                    <Typography variant="h6" className="w-3/12 py-1">Action</Typography>
+                                    <Typography variant="h6" className="w-36 md:flex-grow py-1">Parameters</Typography>
+                                    <IconButton className="w-2/12 hover:bg-transparent"><AddCircle /></IconButton>
                                 </div>
-                                <div className="w-full flex gap-1 justify-between items-center">
                                 {
-                                    data.data.map(item => (
-                                        <>
-                                        <FormControl variant="outlined" size="small">
-                                            <Select value={item.actions[0]}>
-                                                {item.actions.map(i => <MenuItem value={i}>{i}</MenuItem>)}
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl variant="outlined" size="small">
-                                            <Select value={item.params[0]}>
-                                                {item.params.map(i => <MenuItem value={i}>{i}</MenuItem>)}
-                                            </Select>
-                                        </FormControl>
-                                        <IconButton><Delete color="primary" /></IconButton>
-                                        </>
-                                    ))
+                                    data.data.map(item => 
+                                        <div className="flex justify-between w-full gap-1 sm:gap-10 h-8 mb-5">
+                                            <div className="w-3/12">
+                                                <select className="w-20 md:w-full bg-gray-100 h-full">
+                                                    {item.actions.map(i => <option value={i}>{i}</option>)}
+                                                </select>
+                                            </div>
+                                            <div className="w-36 md:flex-grow">
+                                                <select className="w-20 md:w-full bg-gray-100 h-full">
+                                                    {item.params.map(i => <option value={i}>{i}</option>)}
+                                                </select>
+                                            </div>
+                                            <IconButton className="w-2/12 hover:bg-transparent"><img src={NegativeIcon} style={{width: 22}} alt="delete" /></IconButton>
+                                        </div>
+                                    )
                                 }
-                                </div>
                             </div>
                             </>
                         )
@@ -70,7 +68,7 @@ export default function DrawerData({data}){
                                                 {item.actions.map(i => <MenuItem value={i}>{i}</MenuItem>)}
                                             </Select>
                                         </FormControl>
-                                        <IconButton><Delete color="primary" /></IconButton>
+                                        <IconButton><img src={NegativeIcon} style={{maxHeight: 22}} alt="delete" /></IconButton>
                                         </>
                                     ))
                                 }
@@ -111,7 +109,7 @@ export default function DrawerData({data}){
                                                 {item.params.map(i => <MenuItem value={i}>{i}</MenuItem>)}
                                             </Select>
                                         </FormControl>
-                                        <IconButton><Delete color="primary" /></IconButton>
+                                        <IconButton><img src={NegativeIcon} style={{maxHeight: 22}} alt="delete" /></IconButton>
                                         </>
                                     ))
                                 }
--- src/components/Console/EditStateTab.js
+++ src/components/Console/EditStateTab.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import {Grid, TextField, Button, Typography, IconButton} from '@material-ui/core';
+import {Grid, TextField, Button, Typography, IconButton, InputBase} from '@material-ui/core';
 import { Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
 
 import clsx from 'clsx';
 
 import AddCircle from '@material-ui/icons/AddCircle';
-import DeleteIcon from '@material-ui/icons/Delete';
+import NegativeIcon from '../../assets/negative.png';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import DrawerData from './DrawerData';
@@ -55,45 +55,49 @@ export default function EditStateTab({data}){
     let [drawerData, setDrawerData] = useState(null);
     return (
         <Grid container direction="column" className="border rounded-lg overflow-scroll sm:overflow-auto">
-            <Grid item className="flex gap-3 p-1 sm:p-3 border-b-2 items-center justify-between flex flex-col sm:flex-row">
-                <TextField variant="outlined" value={name} onChange={(e)=>{}} label="Enter State Name" size="small" />
-                <div className="flex gap-1 sm:justify-end w-full items-center justify-around">
-                    <Button variant="contained" color="secondary">Cancel</Button>
-                    <Button variant="contained" color="primary">Delete</Button>
+            <Grid item className="flex gap-3 p-2 sm:p-3 border-b items-center justify-between flex flex-col md:flex-row">
+                <InputBase 
+                    placeholder="Enter State Name"
+                    value={name}
+                    className="w-full md:w-6/12 px-2 bg-gray-100 h-10 rounded-lg"
+                />
+                <div className="flex gap-9 md:justify-end w-full md:w-5/12 items-center justify-between">
+                    <Button variant="contained" color="secondary" className="w-36">Cancel</Button>
+                    <Button variant="contained" color="primary" className="w-36">Save</Button>
                 </div>
             </Grid>
-            <Grid item className="p-1 sm:p-6 flex relative gap-3">
-                <div className="border rounded-lg w-full sm:w-50 md:w-20 bg-gray-200 flex flex-col sm:flex-grow">
+            <Grid item className="pl-1 md:pl-10 flex relative md:gap-20">
+                <div className="border rounded-lg md:w-72 sm:w-36 bg-gray-200 flex flex-col divide-y my-9">
                     <div className="p-1 sm:p-3 text-center flex flex-col flex-grow">
                         <Hidden smUp><IconButton className="self-end"><ChevronLeftIcon className="bg-gray-400" onClick={()=>setOpen(true)} /></IconButton></Hidden>
-                        <Typography variant="body1">{name}</Typography>
+                        <span className="self-center">{name ? name : '...'}</span>
                     </div>
-                    <div className="p-1 sm:px-3 flex flex-col flex-grow bg-white items-center border-t-2 border-b-2 border-gray-200">
+                    <div className="p-1 sm:px-3 flex flex-col flex-grow bg-white items-center">
                         <div className="flex items-center justify-between w-full">
                             <p className="flex-grow p-2 rounded-lg">onEnter</p><span><IconButton><AddCircle /></IconButton></span>
                         </div>
                         {   onEnterFunctions.map((item, index) => (
-                            <div className="flex items-center justify-between w-full hover:bg-gray-100" onClick={()=>setDrawerData(onEnterFunctions[index])}>
-                                <TextField variant="filled" size="small" value={item.name} />
-                                <span><IconButton><DeleteIcon color="secondary" /></IconButton></span>
+                            <div className="flex items-center justify-between w-full hover:bg-pink-100 md:gap-8 gap-2" onClick={()=>setDrawerData(onEnterFunctions[index])}>
+                                <InputBase className="bg-gray-100 w-full px-2 rounded-lg" value={item.name} />
+                                <IconButton><img src={NegativeIcon} alt="delete" style={{maxHeight: 22}} /></IconButton>
                             </div>
                             ))
                         }
                     </div>
-                    <div className="p-1 sm:px-3 flex flex-col flex-grow bg-white items-center rounded-lg">
+                    <div className="p-1 sm:px-3 flex flex-col flex-grow bg-white items-center rounded-b-lg">
                         <div className="flex items-center justify-between w-full">
                             <p className="flex-grow p-2 rounded-lg">onInput</p><span><IconButton><AddCircle /></IconButton></span>
                         </div>
                         {   onInputFunctions.map((item, index) =>(
-                            <div className="flex items-center justify-between w-full hover:bg-gray-100" onClick={()=>setDrawerData(onInputFunctions[index])}>
-                                <TextField variant="filled" size="small" value={item.name} />
-                                <span><IconButton><DeleteIcon color="secondary" /></IconButton></span>
+                            <div className="flex items-center justify-between w-full hover:bg-pink-100 md:gap-8 gap-2" onClick={()=>setDrawerData(onInputFunctions[index])}>
+                                <InputBase className="bg-gray-100 w-full px-2 rounded-lg" value={item.name} />
+                                <IconButton><img src={NegativeIcon} alt="delete" style={{maxHeight: 22}} /></IconButton>
                             </div>
                             ))
                         }
                     </div>
                 </div>
-                <div className={clsx({["bg-white absolute sm:relative sm:w-50 md:w-80 sm:ml-0 flex-grow flex flex-col rounded-lg border-gray-200 border"]:true, [classes.drawerOpen]: open, [classes.drawerClose]:!open})}>
+                <div className={clsx({["bg-white absolute md:relative sm:ml-0 flex-grow flex flex-col border-gray-200 border-l"]:true, [classes.drawerOpen]: open, [classes.drawerClose]:!open})}>
                     <Hidden smUp><IconButton className="self-start"><ChevronRightIcon className="bg-gray-400" onClick={()=>setOpen(false)}/></IconButton></Hidden>
                     <DrawerData data={drawerData} />
                 </div>
