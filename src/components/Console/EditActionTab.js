a8bd12d76f500c7149e34736c472b5a82fb9ba7e
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-09-29 11:05:18 +0500

    Updated styles of ActionTabs

--- src/components/Console/EditActionTab.js
+++ src/components/Console/EditActionTab.js
@@ -1,5 +1,5 @@
 
-import { Grid, Button, Typography, IconButton, InputBase } from '@material-ui/core';
+import { Grid, Button, Typography, IconButton, InputBase, TableContainer, Table, TableHead, TableBody, TableCell, TableRow } from '@material-ui/core';
 import AddCircle from '@material-ui/icons/AddCircle';
 import NegativeIcon from '../../assets/negative.png';
 
@@ -7,47 +7,57 @@ export default function AddEditTab(props) {
     let action = props.action;
     return (
         <Grid container direction="column" className="border rounded-lg overflow-scroll sm:overflow-auto bg-white h-full">
-            <Grid item className="flex gap-3 p-2 sm:p-3 border-b items-center justify-between flex flex-col md:flex-row">
+            <Grid item className="gap-3 p-2 sm:p-3 border-b items-center justify-between flex flex-col md:flex-row">
                 <InputBase
                     label="Enter command Name"
                     value={action.name}
                     className="w-full md:w-6/12 px-2 bg-gray-100 h-10 rounded-lg"
                 />
                 <div className="flex gap-9 md:justify-end w-full md:w-5/12 items-center justify-between">
-                    <Button variant="contained" color="secondary" className="w-36">Delete</Button>
-                    <Button variant="contained" color="primary" className="w-36">Save</Button>
+                    <Button variant="contained" color="secondary" className="md:w-36">Delete</Button>
+                    <Button variant="contained" color="primary" className="md:w-36">Save</Button>
                 </div>
             </Grid>
-            <Grid item className="p-1 sm:p-3">
+            <Grid item className="p-2 sm:p-3 w-full">
                 <Grid container direction="column">
-                    <Grid item className="flex flex-col">
-                        <Typography variant="h4">Parameters List</Typography>
+                    <Grid item className="flex flex-col w-full">
+                        <Typography className="text-2xl font-bold md:text-4xl">Parameters List</Typography>
                     </Grid>
-                    <Grid item container direction="column" className="mt-8">
-                        <Grid item className="flex w-full gap-4 md:gap-24">
-                            <Typography variant="h6" className="w-36 md:flex-grow py-1">Name</Typography>
-                            <Typography variant="h6" className="w-36 md:flex-grow py-1">Data type</Typography>
-                            <IconButton className="w-2/12 hover:bg-transparent"><AddCircle /></IconButton>
-                        </Grid>
-                        {
-                            [1].map(item => <>
-                                <Grid item className="flex justify-between w-full gap-1 md:gap-24 h-8 mb-5">
-                                    <div className="w-36 md:flex-grow">
-                                        <InputBase
-                                            className="bg-gray-100 md:w-full h-full px-2"
-                                        />
-                                    </div>
-                                    <div className="w-36 md:flex-grow">
-                                        <select className="w-20 md:w-full bg-gray-100 h-full">
-                                            <option value="String">String</option>
-                                            <option value="Number">Number</option>
-                                            <option value="Boolean">Boolean</option>
-                                        </select>
-                                    </div>
-                                    <IconButton className="w-2/12 hover:bg-transparent"><img src={NegativeIcon} style={{ width: 22 }} alt="delete" /></IconButton>
-                                </Grid>
-                            </>)
-                        }
+                    <Grid item container direction="column" className="mt-8 overflow-auto w-full">
+                        <TableContainer className="w-full">
+                            <Table size="small">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell className="border-b-0 px-0.5 md:px-3"><Typography color="primary" className="text-sm md:text-lg font-bold">Name</Typography></TableCell>
+                                        <TableCell className="border-b-0 px-0.5 md:px-3"><Typography color="primary" className="text-sm md:text-lg font-bold">Data type</Typography></TableCell>
+                                        <TableCell className="px-0.5 md:px-3 border-b-0 w-4 last:pr-0"><IconButton><AddCircle /></IconButton></TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {
+                                        [1,2,3].map(item => (
+                                            <TableRow>
+                                                <TableCell className="px-0.5 md:px-3 my-1 border-b-0">
+                                                    <InputBase
+                                                        className="bg-gray-100 md:w-full lg:w-10/12 h-full px-2"
+                                                    />
+                                                </TableCell>
+                                                <TableCell className="px-0.5 md:px-3 my-1 border-b-0">
+                                                    <select className="w-20 md:w-full bg-gray-100 h-9">
+                                                        <option value="English">English</option>
+                                                        <option value="French">French</option>
+                                                        <option value="Italian">Italian</option>
+                                                    </select>
+                                                </TableCell>
+                                                <TableCell className="px-0.5 md:px-3 my-1 border-b-0 w-4 last:pr-0">
+                                                    <IconButton><img src={NegativeIcon} style={{width: 22}} alt="delete" /></IconButton>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))
+                                    }
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
                     </Grid>
                 </Grid>
             </Grid>
